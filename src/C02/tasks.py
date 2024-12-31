7cdb655f3980fe664c1b7e8614540a5f76f4899c
= <=>
2025-01-01 02:59:52 +0300

    EP03

--- src/C02/tasks.py
+++ src/C02/tasks.py
@@ -1,9 +1,16 @@
 from celery import Celery
 import time
 
-app = Celery("tasks", broker="redis://localhost:6379/0")
+app = Celery("tasks", backend="redis://localhost:6379/0")
+
+app.conf.broker_url = "redis://localhost:6379/0"
 
 @app.task
 def send_otp(phone_no, otp):
     time.sleep(5)
+    return True
+
+@app.task
+def custom_exception():
+    raise Exception("ZalCoders")
     return True
